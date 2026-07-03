aebb7b4ba402dc1be49e560e531cf632acdc80a4
JADAVDHRUVIT21 <dhruvit715@gmail.com>
2026-07-03 09:58:54 +0530

    refactor: remove unused components and styles from the frontend
    - Deleted App.css, App.jsx, index.css, main.jsx, and vite.config.js as they contained unused styles and components.
    - Removed assets: hero.png, react.svg, and vite.svg to clean up the project.
    - Added vercel.json for routing configuration.

--- SmartERP/backend/app/database/db.py
+++ SmartERP/backend/app/database/db.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -9,7 +10,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL =", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
-print("DATABASE_URL =", DATABASE_URL)
+
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
