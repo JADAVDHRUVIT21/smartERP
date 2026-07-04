dd1f5066c96fa4f468ad6626e421ec1790733a5a
JADAVDHRUVIT21 <dhruvit715@gmail.com>
2026-07-04 10:23:40 +0530

    Add @vercel/speed-insights dependency and clean up console log in axios.js

--- SmartERP/frontend/src/api/axios.js
+++ SmartERP/frontend/src/api/axios.js
@@ -4,6 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-console.log("API base url:", import.meta.env.VITE_API_URL);
 
+console.log(import.meta.env.VITE_API_URL)
 export default API;
